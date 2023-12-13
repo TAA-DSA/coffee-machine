374ffa36e07b973fefc9f9dcba5a602b36441ea6
Tasdeed <ayonaziz@gmail.com>
2023-12-13 15:08:00 -0500

    coffee machine complete

--- main.py
+++ main.py
@@ -30,11 +30,42 @@ resources = {
     "coffee": 100,
 }
 
-#TODO: Print report
-
-dict_values = MENU['espresso'].values()
-print(dict_values)
 resources['money'] = 0
+
+# ask_prompt = input("What would you like? (espresso/latte/cappuccino) : ").lower()
+# def report(ask_prompt, resources):
+
+def is_sufficient_resources(ingredients):
+
+    for items in ingredients:
+        if ingredients[items] >= resources[items]:
+            print(f'Sorry not enough {ingredients[items]}')
+            return False
+        return True
+
+
+def cost_check(ask_quarters, ask_dimes, ask_nickles, ask_pennies, drink):
+
+    # print(ask_quarters, ask_dimes, ask_nickles, ask_pennies, drink)
+    total = (0.25 * ask_quarters) + (0.10 * ask_dimes) + (0.05 * ask_nickles) + (0.01 * ask_pennies)
+    if total < drink:
+        print('Sorry not enough money. Money refunded')
+        return False
+    elif total == drink:
+        resources['money'] += drink
+        return True
+    else:
+        refund = total - drink
+        resources['money'] += drink
+        print(f'Here is ${refund} in change')
+        return True
+
+def make_coffee(drink, ask_prompt):
+    for values in drink:
+        resources[values] -= drink[values]
+    print(f'Here is your {ask_prompt}. Enjoy!')
+
+
 def coffee_machine_start():
 
     start_machine = True
@@ -58,12 +89,24 @@ def coffee_machine_start():
                 elif selected_items == 'Money':
                     print(f'{selected_items} : $ {value} ')
 
-        if ask_prompt == 'espresso' or ask_prompt == 'latte' or ask_prompt == 'cappuccino':
-            #ask user for money in quarter, dimes, nickles, pennies
-            for key, value in MENU[ask_prompt].items():
-                # check if resources are available to make the chosen drink
+        elif ask_prompt == 'espresso' or ask_prompt == 'latte' or ask_prompt == 'cappuccino':
 
-                print(key, value)
+            #ask user for money in quarter, dimes, nickles, pennies
+            drink = MENU[ask_prompt]
+            if (is_sufficient_resources(drink['ingredients'])):
+                print('Please insert coins')
+                ask_quarters = float(input('how many quarters?: '))
+                ask_dimes = float(input('how many dimes?: '))
+                ask_nickles = float(input('how many nickles?: '))
+                ask_pennies = float(input('how many pennies?: '))
+                if (cost_check(ask_quarters, ask_dimes, ask_nickles, ask_pennies, drink['cost'])):
+                    make_coffee(drink['ingredients'], ask_prompt)
+            else:
+                print('Not enough resources available')
+
+
+
+
         elif ask_prompt == "off":
             start_machine = False
     if not start_machine:
