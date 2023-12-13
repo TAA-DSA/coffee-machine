2b0e2dbadf371a52fabb04b1eb6678b32582b8f5
Tasdeed <ayonaziz@gmail.com>
2023-12-13 12:01:05 -0500

    generate report completed'

--- main.py
+++ main.py
@@ -30,23 +30,40 @@ resources = {
     "coffee": 100,
 }
 
+#TODO: Print report
+
 dict_values = MENU['espresso'].values()
 print(dict_values)
-resources['Money'] = 0
+resources['money'] = 0
 def coffee_machine_start():
+
     start_machine = True
     while start_machine:
         ask_prompt = input("What would you like? (espresso/latte/cappuccino) : ").lower()
-        if ask_prompt == 'espresso' or ask_prompt == 'latte' or ask_prompt == 'cappuccino':
-            for keys, value in MENU[ask_prompt].items():
-                #check if resources are available to make the chosen drink
-                print(keys, value)
-        elif ask_prompt == 'report':
+
+        if ask_prompt == 'report':
             # loop through the dictionary using items to print the key value pair
+            # add units to values
             for items, value in resources.items():
+                item_units = {
+                    'Water': 'ml',
+                    'Milk': 'ml',
+                    'Coffee': 'mg',
+                }
+                selected_items = items[0].upper() + items[slice(1, len(items))]
 
+                if selected_items in item_units:
+                    unit = item_units[selected_items]
+                    print(f'{selected_items} : {value} {unit}')
+                elif selected_items == 'Money':
+                    print(f'{selected_items} : $ {value} ')
 
-                print(f'{items}: {value} ')
+        if ask_prompt == 'espresso' or ask_prompt == 'latte' or ask_prompt == 'cappuccino':
+            #ask user for money in quarter, dimes, nickles, pennies
+            for key, value in MENU[ask_prompt].items():
+                # check if resources are available to make the chosen drink
+
+                print(key, value)
         elif ask_prompt == "off":
             start_machine = False
     if not start_machine:
